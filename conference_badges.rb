e37fcc2a50d3624ea39918beb0534638d8eced83
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-13 23:51:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 # Write your code here.
-def badge_maker(name) {
+def badge_maker(name)
   return "Hello, my name is #{name}"
-}
+end
 
-def batch_badge_creator(names) {
+def batch_badge_creator(names)
   badges = Array.new
   names.each do |name|
     badge << badge_maker(name)
   end
   return badges
-}
+end
