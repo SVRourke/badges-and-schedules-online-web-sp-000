02726ddd07338bea8b2862d6ac642167cc3ed9ec
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-13 23:47:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name) {
   "Hello, my name is #{name}"
 }
 
-def batch_badge_creator(names) {
-  names.each {|name| badge_maker(name)} 
-}
+# def batch_badge_creator(names) {
+#   names.each {|name| badge_maker(name)} 
+# }
