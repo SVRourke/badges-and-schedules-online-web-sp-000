0091a87a5ed18bd4bbca34967c6dfce928fc6593
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 00:05:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,12 @@ def assign_rooms(names)
 end
 
 def printer(names)
-  puts batch_badge_creator(names)
-  puts assign_rooms(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
+  assign_rooms(names).each do |pair|
+    puts pair
+  end
 end
 
 printer(['s', 'r', 'm', 'j'])
